277cca2efa497d44e62a5d9361f712f302aa59cf
Omar Khanfer <omar.aljaber@gmail.com>
2012-11-25 01:26:18 +0400

    upload website

---
+++ Scripts/viewModel.js
@@ -0,0 +1,18 @@
+﻿function viewModel() {
+    var _datepicker = {
+        init: function (element, valueAccessor, allBinding, viewModel, context) {
+            debugger;
+            var options = {
+                showOtherMonths: true,
+                selectOtherMonths: true
+            };
+            if (typeof valueAccessor() === 'object') {
+                $.extend(options, valueAccessor());
+            }
+            $(element).datepicker(options);
+        }
+    }
+
+    ko.bindingHandlers.datepicker = _datepicker;
+}
+
